44923e58b1ed4c75e8246d63243e4658d4907d4c
402861037@163.com <testpoo>
2023-04-18 17:23:00 +0800

    修复已知缺陷

--- lunars.py
+++ lunars.py
@@ -6,25 +6,25 @@ from tkinter.ttk import *
 import os
 from lunardata import *
 
-__author__ = {'name' : 'TestPoo', 'created' : '2022-05-05', 'modify' : '2022-11-14'}
+__author__ = {'name' : 'TestPoo', 'created' : '2022-05-05', 'modify' : '2023-04-18'}
 
 #**实现界面功能****
 class Application_ui(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master.title('万年历')
-        self.master["bg"]='#fff'
+        self.master["bg"]='#f5f5f5'
         self.createWidgets()
         self.position()
 
     def position(self):
         top.resizable(False,False)
         curWidth = 350
-        curHeight = 458
+        curHeight = 470
         scnWidth,scnHeight = top.maxsize()
-        tmpplace = '%dx%d-%d-%d'%(curWidth,curHeight,0,30)
+        tmpplace = '%dx%d-%d-%d'%(curWidth,curHeight,2,30)
         top.geometry(tmpplace)
-        #top.attributes('-type', 'dock')
+        top.attributes('-type', 'dock')
         top.attributes("-topmost", True)
         top.focus_force()
 
@@ -35,27 +35,27 @@ class Application_ui(Frame):
 
         self.style = Style()
         self.style.theme_use('default')
-        self.style.configure("Pn.TButton",foreground="#303133",background="#fff",borderwidth=0,font=('',12),justify='center',relief=FLAT)
-        self.style.configure("To.TButton",foreground="#2ca7f8",background="#fff",borderwidth=0,font=('',12),justify='center',relief=FLAT)
-        self.style.configure("Yd.TLabel",foreground="#161616",background="#fff",borderwidth=0,font=('',12,'bold'),anchor=CENTER)
-        self.style.configure("Ym.TLabel",foreground="#161616",background="#fff",borderwidth=0,font=('',12),anchor=CENTER)
-        self.style.configure("Wk.TLabel",foreground="#363636",background="#eee",borderwidth=0,font=('',10),anchor=CENTER)
+        self.style.configure("Pn.TButton",foreground="#303133",background="#f5f5f5",borderwidth=0,font=('',12),justify='center',relief=FLAT)
+        self.style.configure("To.TButton",foreground="#2ca7f8",background="#f5f5f5",borderwidth=0,font=('',12),justify='center',relief=FLAT)
+        self.style.configure("Yd.TButton",foreground="#161616",background="#f5f5f5",borderwidth=0,font=('',12),justify='center',relief=FLAT)
+        self.style.configure("Ym.TButton",foreground="#161616",background="#f5f5f5",borderwidth=0,font=('',12),anchor='center',relief=FLAT)
+        self.style.configure("Wk.TButton",foreground="#363636",background="#f5f5f5",borderwidth=0,font=('',10),anchor='center',relief=FLAT)
 
-        Label(top, text="  "+self.cdate+"\n  "+self.clunar,style="Yd.TLabel").place(x=0,y=0,width=350,height=60)
+        Button(top, text="  "+self.cdate+"\n  "+self.clunar,style="Yd.TButton").place(x=0,y=0,width=350,height=60)
         Separator(top,orient=HORIZONTAL).place(x=0,y=60,width=350,height=2)
 
         self.preYear = Button(top,text="◀",command = self.pre_year,style="Pn.TButton").place(x=0,y=62,width=30,height=48)
-        self.cyear = Label(top, text=self.cdate[0:5],style="Ym.TLabel")
+        self.cyear = Button(top, text=self.cdate[0:5],style="Ym.TButton")
         self.cyear.place(x=30,y=62,width=80,height=48)
         self.nextYear = Button(top,text="▶",command = self.next_year,style="Pn.TButton").place(x=110,y=62,width=30,height=48)
         self.preMonth = Button(top,text="◀",command = self.pre_month,style="Pn.TButton").place(x=140,y=62,width=30,height=48)
-        self.cmonth = Label(top, text=self.cdate[5:8],style="Ym.TLabel")
+        self.cmonth = Button(top, text=self.cdate[5:8],style="Ym.TButton")
         self.cmonth.place(x=170,y=62,width=60,height=48)
         self.nextMonth = Button(top,text="▶",command = self.next_month,style="Pn.TButton").place(x=230,y=62,width=30,height=48)
         self.today = Button(top,text="返回今天",command = self.today,style="To.TButton").place(x=260,y=62,width=100,height=48)
 
         for i in range(len(self.cweek)):
-            Label(top,text='周' + self.cweek[i],style="Wk.TLabel").place(x=50*i,y=110,width=50,height=50)
+            Button(top,text='周' + self.cweek[i],style="Wk.TButton").place(x=50*i,y=110,width=50,height=50)
         
         self.button_list = []
         self.addButton(self.cday)
@@ -65,10 +65,10 @@ class Application_ui(Frame):
     def addButton(self,cday):
 
         self.style.configure(".TButton",foreground="#fff",background="#2ca7f8",borderwidth=0,font=('',10,'bold'),justify='center',relief=FLAT)
-        self.style.configure("*.TButton",foreground="#a8a8a8",background="#fff",borderwidth=0,font=('',10),justify='center',relief=FLAT)
-        self.style.configure("@.TButton",foreground="#e7493e",background="#fff",borderwidth=0,font=('',10,'bold'),justify='center',relief=FLAT)
-        self.style.configure("$.TButton",foreground="#2ca7f8",background="#fff",borderwidth=0,font=('',10,'bold'),justify='center',relief=FLAT)
-        self.style.configure("A.TButton",foreground="#363636",background="#fff",borderwidth=0,font=('',10),justify='center',relief=FLAT)
+        self.style.configure("*.TButton",foreground="#a8a8a8",background="#f5f5f5",borderwidth=0,font=('',10),justify='center',relief=FLAT)
+        self.style.configure("@.TButton",foreground="#e7493e",background="#f5f5f5",borderwidth=0,font=('',10,'bold'),justify='center',relief=FLAT)
+        self.style.configure("$.TButton",foreground="#2ca7f8",background="#f5f5f5",borderwidth=0,font=('',10,'bold'),justify='center',relief=FLAT)
+        self.style.configure("A.TButton",foreground="#363636",background="#f5f5f5",borderwidth=0,font=('',10),justify='center',relief=FLAT)
         for i in range(len(self.cday)):
             m,n = divmod(i,7)
             if cday[i].split('\n')[0].strip() == str(datetime.now().day):
